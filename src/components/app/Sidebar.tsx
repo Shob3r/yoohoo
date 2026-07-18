e81f8c124fbce2223c0e46f299eac280326a980e
Chakorn Kungsanarux <chakornk2007@gmail.com>
2026-07-17 23:07:22 -0700

    perf: optimize Sidebar styles

--- src/components/app/Sidebar.tsx
+++ src/components/app/Sidebar.tsx
@@ -8,17 +8,16 @@ export const Sidebar = () => {
 
 	return (
 		<motion.div
-			style={{ translateX: "-20%" }}
-			class="absolute top-0 right-0 bottom-0 z-20 flex h-auto max-h-100 flex-col items-center justify-center gap-y-4 self-center overflow-y-scroll rounded-xl bg-black/80 p-4"
+			class="absolute top-0 right-4 bottom-0 z-20 flex h-auto max-h-100 w-20 flex-col items-center justify-center gap-y-4 self-center overflow-y-scroll rounded-xl bg-black/80 p-4"
 		>
 			{resolvedAssets &&
 				Object.entries(resolvedAssets).map(([key, data]) => (
 					<button
 						type="button"
 						key={key}
-						class="relative h-12 w-12 cursor-pointer rounded-lg border-white transition-transform duration-150 hover:scale-110 active:scale-90"
+						class="relative h-12 w-12 cursor-pointer rounded-lg border-2 transition-transform duration-150 hover:scale-110 active:scale-90"
 						style={{
-							borderWidth: game === +key ? "0.125rem" : "",
+							borderColor: game === +key ? "#fff" : "#fff0",
 						}}
 						onClick={() => setGame(+key)}
 					>
