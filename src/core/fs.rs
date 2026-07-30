c8094953f587a4ad93ee171a5712c05d1046f651
Shob3r <62476902+Shob3r@users.noreply.github.com>
2026-07-29 23:25:43 -0700

    feat(fs): complete fs rust reimplementation

--- src/core/fs.rs
+++ src/core/fs.rs
@@ -1,8 +1,31 @@
-use std::{fs, path::PathBuf};
+use flate2::read::GzDecoder as Gz;
+use fs_extra::dir::get_size;
+use std::{
+    fs::{self, create_dir_all},
+    path::PathBuf,
+};
+use tar::Archive as Tar;
+use xz::read::XzDecoder as Xz;
+use zip::ZipArchive as Zip;
+use zstd::Decoder as Zstd;
 
-use anyhow::{Context, Error, Ok, Result};
+use anyhow::{Context, Error, Ok, Result, ensure};
 use directories::BaseDirs;
+use log::warn;
 
+/// Directories that elysiae commonly uses, provided to make filesystem
+/// operations a bit cleaner by only requiring paths relative to these base
+/// directories
+///
+/// Paths:
+///
+/// AppData: ~/.local/share/elysiae
+///
+/// Desktop: ~/Desktop
+///
+/// Home: ~
+///
+/// Compat: ~/.local/share/elysiae/proton-data
 pub enum BaseDirectory {
     AppData,
     Desktop,
@@ -10,60 +33,135 @@ pub enum BaseDirectory {
     Compat,
 }
 
-/// Wrapper function for std::fs::exists(), but relative to a specific "Base
-/// Directory"
+/// Used for getting paths and base directories for filesystem operations that
+/// require two paths instead of one
+pub struct MultiPathOptions {
+    init_path: PathBuf,
+    init_path_base_dir: Option<BaseDirectory>,
+    dest_path: PathBuf,
+    dest_path_base_dir: Option<BaseDirectory>,
+    overwrite: Option<bool>,
+}
+
+/// Wrapper function for std::fs::exists(), but relative to a base directory
 ///
-/// If no "Base Directory" is supplied, the function defaults to the app data
+/// If no base directory is provided, the function defaults to the app data
 /// directory (~/.local/share/elysiae)
 pub fn exists(p: PathBuf, base_dir: Option<BaseDirectory>) -> Result<bool> {
     let fp = full_path(Some(p), base_dir).context("Full path could not be resolved")?;
-    Ok(fs::exists(fp).context("exists() could not complete successfully")?)
+    Ok(fp.try_exists()?)
 }
 
-/// Wrapper function for std::fs::read, relative to a specified "Base Directory"
+/// Wrapper function for std::fs::read, relative to a base directory
 ///
-/// If no "Base Directory" is supplied, the function defaults to the app data
+/// If no base directory is provided, the function defaults to the app data
 /// directory (~/.local/share/elysiae)
 pub fn read_file(p: PathBuf, base_dir: Option<BaseDirectory>) -> Result<Vec<u8>> {
     let fp = full_path(Some(p), base_dir)?;
-    if fp.exists() {
-        let data = fs::read(fp).context("Could not read this file")?;
+    ensure!(
+        fp.try_exists()?,
+        "The Path \"{}\" could not be found on disk",
+        fp.to_string_lossy()
+    );
 
-        Ok(data)
-    } else {
-        Err(Error::msg(format!(
-            "The Path \"{}\" could not be found on disk",
-            fp.to_string_lossy()
-        )))
-    }
+    let data = fs::read(fp).context("Could not read this file")?;
+    Ok(data)
 }
 
-/// Wraper function for std::fs::write, but relative to a specified user
+/// Wraper function for std::fs::write, but relative to a base
 /// directory
 ///
-/// If no user directory is supplied, the function defaults to the app data
+/// If no base directory is provided, the function defaults to the app data
 /// directory (~/.local/share/elysiae)
 pub fn write_file(p: PathBuf, contents: &[u8], base_dir: Option<BaseDirectory>) -> Result<()> {
     let fp = full_path(Some(p), base_dir)?;
+    ensure!(
+        fp.try_exists()?,
+        "The Path \"{}\" could not be found on disk",
+        fp.to_string_lossy()
+    );
 
-    if fp.exists() {
-        let _ = fs::write(fp, contents).context("Failed to write to the path")?;
+    ensure!(
+        !fp.is_dir(),
+        "The Path \"{}\" is a directory",
+        fp.to_string_lossy()
+    );
 
+    // Create all directories on the path that don't exist
+    let parent_dir = fp.parent().unwrap();
+    if !parent_dir.try_exists()? {
+        let _ = create_dir_all(parent_dir);
+    }
+
+    // Write the contents of the file to the final location
+    let _ = fs::write(fp, contents).context("Failed to write to the path")?;
+
+    Ok(())
+}
+
+/// Wrapper function for std::fs::create_dir_all, relative to a base directory
+///
+/// If the path this function is trying to create exists, the function will emit
+/// a warning but still completes successfully, as it didn't run into any real
+/// errors
+///
+/// If no base directory is provided, the function defaults to the app data
+/// directory (~/.local/share/elysiae)
+pub fn mkdir(p: PathBuf, base_dir: Option<BaseDirectory>) -> Result<()> {
+    let fp = full_path(Some(p), base_dir).context("File path could not be resolved")?;
+
+    if fp.try_exists()? {
+        warn!(
+            "The path \"{}\" already exists. No action taken",
+            fp.to_string_lossy()
+        );
         Ok(())
     } else {
-        Err(Error::msg(format!(
-            "The Path \"{}\" could not be found on disk",
-            fp.to_string_lossy()
-        )))
+        fs::create_dir_all(fp).context("The path could not be created")?;
+        Ok(())
     }
 }
 
-fn dirOf(p: PathBuf) -> Result<PathBuf> {
-    unimplemented!()
+/// Wrapper function for std::fs::rename(), relative to a base directory
+///
+/// Both paths must be of the same type, if they are different an error will be
+/// thrown
+///
+/// If no base directory is provided, the function defaults to the app data
+/// directory (~/.local/share/elysiae)
+pub fn rename(options: MultiPathOptions) -> Result<()> {
+    let ifp = full_path(Some(options.init_path), options.init_path_base_dir)
+        .context("Could not resolve initial path")?;
+    let dfp = full_path(Some(options.dest_path), options.dest_path_base_dir)
+        .context("Could not resolve destination path")?;
+
+    ensure!(
+        ifp.try_exists()?,
+        "The initial path \"{}\" does not exist",
+        ifp.to_string_lossy()
+    );
+
+    let allow_overwrites = options.overwrite.unwrap_or(true);
+
+    ensure!(
+        !dfp.try_exists()? || allow_overwrites,
+        "The destination path \"{}\" already exists and overwriting files has been disabled!",
+        dfp.to_string_lossy()
+    );
+
+    ensure!(
+        (ifp.is_dir() && dfp.is_dir()) || (ifp.is_file() && dfp.is_file()),
+        "{} and {} are two different item types",
+        ifp.to_string_lossy(),
+        dfp.to_string_lossy()
+    );
+
+    fs::rename(ifp, dfp)?;
+    Ok(())
 }
 
-/// Wraper function for std::fs::remove_file, std::fs::remove_dir and
-/// std::fs::remove_dir_all, relative to a specified "Base Directory"
+/// Wrapper function for std::fs::remove_file, std::fs::remove_dir and
+/// std::fs::remove_dir_all, relative to a base directory
 ///
 /// The function automatically determines weather the path you specified is a
 /// directory or not and calls the appropriate std function to remove the item
@@ -72,11 +170,11 @@ fn dirOf(p: PathBuf) -> Result<PathBuf> {
 /// If a recursive parameter is not provided, it will automatically recursively
 /// delete a directory
 ///
-/// If a "Base Directory" is not provided, it will default to the app data
+/// If no base directory is provided, the function will default to the app data
 /// directory (~/.local/share/elysiae)
 pub fn remove(p: PathBuf, base_dir: Option<BaseDirectory>, recursive: Option<bool>) -> Result<()> {
     let fp = full_path(Some(p), base_dir)?;
-    if fp.exists() {
+    if fp.try_exists()? {
         if fp.is_file() {
             fs::remove_file(fp).context("Could not remove this file")?;
         } else if fp.is_dir() {
@@ -104,11 +202,94 @@ pub fn remove(p: PathBuf, base_dir: Option<BaseDirectory>, recursive: Option<boo
     }
 }
 
-/// Joins a specified "Base directory" with a relative path
+/// Extracts a .tar.gz, .tar.xz, .tar.zstd, or .zip file to a specified
+/// directory.
 ///
-/// If no "relative path" is specified, the "base directory" is returned by
-/// itself If no "base directory is specified", the function will use the app
-/// data directory by default (~/.local/share/elysiae)
+/// Can automatically "flatten" an extracted file (move all items in a nested
+/// directory after extraction one level up), and does so by default. to disable
+/// this behaviour, set the flatten parameter to Some(false)
+///
+/// Both the archive path and destination folder are paths relative to a base
+/// directory. Paths that don't provide a base directory parameter default to
+/// the app data directory (~/.local/share/elysiae)
+pub fn extract_file(options: MultiPathOptions, flatten: Option<bool>) -> Result<()> {
+    let ifp = full_path(Some(options.init_path), options.init_path_base_dir)
+        .context("Could not resolve initial path")?;
+    let dfp = full_path(Some(options.dest_path), options.dest_path_base_dir)
+        .context("Could not resolve destination path")?;
+
+    let should_flatten = flatten.unwrap_or(true);
+
+    ensure!(
+        ifp.try_exists()?,
+        "The initial path \"{}\" does not exist",
+        ifp.to_string_lossy()
+    );
+
+    let allow_overwrites = options.overwrite.unwrap_or(true);
+
+    ensure!(
+        !dfp.try_exists()? || allow_overwrites,
+        "The destination path \"{}\" already exists and overwriting files has been disabled!",
+        dfp.to_string_lossy()
+    );
+
+    let ifp_ext = ifp.extension().unwrap().to_str().unwrap().to_owned();
+    let file = fs::File::open(ifp).context("Could not open the initial path")?;
+
+    // Check for comman alternate file extensions of archive files handled by
+    // Elysiae Elysiae only deals with tarball archives, so there's no need to
+    // check if they are standalone archived files
+    if ifp_ext.eq("gz") || ifp_ext.eq("tgz") {
+        // Borrow dfp to allow the extracted dir flattening to properly execute later
+        Tar::new(Gz::new(file)).unpack(&dfp)?
+    } else if ifp_ext.eq("xz") || ifp_ext.eq("txz") {
+        Tar::new(Xz::new(file)).unpack(&dfp)?;
+    } else if ifp_ext.eq("zst") || ifp_ext.eq("zstd") {
+        Tar::new(Zstd::new(file)?).unpack(&dfp)?;
+    } else if ifp_ext.eq("zip") {
+        Zip::new(file)?.extract(&dfp)?;
+    }
+
+    if should_flatten {
+        let entries: Vec<_> = std::fs::read_dir(&dfp)?.collect::<Result<_, _>>()?;
+
+        if entries.len() == 1 && entries[0].path().is_dir() {
+            let inner_dir = entries[0].path();
+
+            for archive_entry in fs::read_dir(&inner_dir)? {
+                let entry = archive_entry?;
+
+                let target = dfp.join(entry.file_name());
+                fs::rename(entry.path(), target)?;
+            }
+
+            fs::remove_dir(inner_dir)?;
+        }
+    }
+
+    Ok(())
+}
+
+/// Gets the size of a directory, relative to a Base directory. Size is returned
+/// in bytes
+///
+/// If no base directory is provided, the function will default to the app data
+/// directory (~/.local/share/elysiae)
+pub fn get_dir_size(p: PathBuf, base_dir: Option<BaseDirectory>) -> Result<u64> {
+    let fp = full_path(Some(p), base_dir).context("The path could not be resolved")?;
+    let size = get_size(fp).context("Could not get size of directory")?;
+
+    Ok(size)
+}
+
+/// Joins a specified base directory with a relative path
+///
+/// If no relative path is provided, the base directory is returned by
+/// itself.
+///
+/// If no base directory is provided, the function will default to the app data
+/// directory (~/.local/share/elysiae)
 pub fn full_path(p: Option<PathBuf>, base_dir: Option<BaseDirectory>) -> Result<PathBuf> {
     let d = BaseDirs::new().context("Could not find base directories!")?;
 
