2718247e281bafdab58e0343bc803fd9d9fb6eb7
Shob3r <62476902+Shob3r@users.noreply.github.com>
2026-08-04 02:13:53 -0700

    chore: update shell exec params

--- src/util/shell.rs
+++ src/util/shell.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 use anyhow::{Context, Result};
 
-pub fn exec_shell(command: String, args: Vec<String>) -> Result<()> {
+pub fn exec_shell(command: &str, args: &[String]) -> Result<()> {
     Command::new(command)
         .args(args)
         .spawn()
